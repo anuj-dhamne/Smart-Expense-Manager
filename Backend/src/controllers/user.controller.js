e1c4d0c04544d3dc3ce33c8819332ea745270708
anuj-dhamne <anujdhamne10@gmail.com>
2025-02-07 23:11:34 +0530

    basic issue may be solved

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -97,7 +97,7 @@ const loginUser=asyncHandler(async (req,res)=>{
  const loggedUser= await User.findById(user._id).select("-password -refreshToken");
 
  const options={
-    httpOnly:true,
+    httpOnly:false,
     secure:true
  }
 
